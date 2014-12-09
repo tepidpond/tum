f755cf00477fb15229f298a217142c198f78e88c
Jeremiah Hyde <tepidpond@gmail.com>
2014-12-09 12:50:43 -0800

    Initial Commit

---
+++ src/java/com/tepidpond/tum/TUM.java
@@ -0,0 +1,20 @@
+package com.tepidpond.tum;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = TUM.MODID, version = TUM.VERSION)
+public class TUM
+{
+    public static final String MODID = "TUM";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
