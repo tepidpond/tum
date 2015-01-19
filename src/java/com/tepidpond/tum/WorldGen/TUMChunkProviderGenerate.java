111521b23e43aa53ef0d3bd30d22cb195de0d5f9
Jeremiah Hyde <tepidpond@gmail.com>
2015-01-19 13:08:10 -0800

    This is broken but promising.

--- src/java/com/tepidpond/tum/WorldGen/TUMChunkProviderGenerate.java
+++ src/java/com/tepidpond/tum/WorldGen/TUMChunkProviderGenerate.java
@@ -23,20 +23,15 @@ public class TUMChunkProviderGenerate extends ChunkProviderGenerate {
 	private byte[] metaBig;
 	
 	public TUMChunkProviderGenerate(World world, long seed, boolean par4) {
-			super(world, seed, par4);
-			worldObj = world;
-			rand = new Random(seed);
-			
-			this.idsTop = new Block[32768];
-			this.idsBig = new Block[16*16*256];
-			this.metaBig = new byte[16*16*256];
-			
-			data = TUMWorldGenData.get(world);
-	}
-	
-	@Override
-	public Chunk provideChunk(int chunkX, int chunkZ)
-	{
+		super(world, seed, par4);
+		worldObj = world;
+		rand = new Random(seed);
+		
+		this.idsTop = new Block[32768];
+		this.idsBig = new Block[16*16*256];
+		this.metaBig = new byte[16*16*256];
+		
+		data = TUMPerWorldData.get(world);
 		if (!data.isHeightMapGenerated()) {
 			lithos = new Lithosphere(
 					data.getMapSize(),
@@ -56,8 +51,13 @@ public class TUMChunkProviderGenerate extends ChunkProviderGenerate {
 			
 			// Save the normalized output.
 			data.setHeightMap(Util.normalizeHeightMapCopy(lithos.getHeightmap()), lithos.getMapSize());
+			data.markDirty();
 		}
-		
+	}
+	
+	@Override
+	public Chunk provideChunk(int chunkX, int chunkZ)
+	{
 		this.rand.setSeed(chunkX * 341873128712L + chunkZ * 132897987541L);
 		
 		Arrays.fill(idsTop, null);
@@ -92,6 +92,34 @@ public class TUMChunkProviderGenerate extends ChunkProviderGenerate {
 		      (field[x1][y0] - field[x0][y0]) * X +
 		      (field[x0][y1] - field[x0][y0]) * Y +
 		      (field[x0][y0] - field[x1][y0] - field[x0][y1] + field[x1][y1]) * X * Y;
+	}
+	
+	private float quadInterpolate(float[] map, int mapSideLength, float X, float Y) {
+		int mapOriginX = (int)Math.floor(X); while (mapOriginX < 0) mapOriginX += mapSideLength; mapOriginX %= mapSideLength;
+		int mapOriginY = (int)Math.floor(Y); while (mapOriginY < 0) mapOriginY += mapSideLength; mapOriginY %= mapSideLength;
+		float mapOffsetX = (float) (X - Math.floor(X));
+		float mapOffsetY = (float) (Y - Math.floor(Y));
+		int x0 = mapOriginX, x1 = mapOriginX, y0 = mapOriginY, y1 = mapOriginY;
+		if (mapOffsetX > mapOriginX) {
+			x1 = Math.min(mapOriginX + 1, mapSideLength - 1);
+		} else if (mapOffsetX < 0) {
+			mapOffsetX = -mapOffsetX;
+			x1 = Math.max(mapOriginX - 1, 0);
+		}
+		if (mapOffsetY > 0) {
+			y1 = Math.min(mapOriginY + 1, mapSideLength - 1);
+		} else if (mapOffsetY < 0) {
+			mapOffsetY = -mapOffsetY;
+			y1 = Math.max(mapOriginY - 1, 0);
+		}
+		int a = y0 * mapSideLength + x0;
+		int b = y0 * mapSideLength + x1;
+		int c = y1 * mapSideLength + x0;
+		int d = y1 * mapSideLength + y1;
+		
+		// quadratic interpolation: a + (b-a)x + (c-a)y + (a-b-c+d)xy
+		return map[a] + (map[b] - map[a]) * mapOffsetX + (map[c] - map[a]) * mapOffsetY +
+				(map[a] - map[b] - map[c] + map[d]) * mapOffsetX * mapOffsetY;
 	}
 	
 	private void generateTerrain(int chunkX, int chunkZ, Block[] idsTop, Random rand, Block[] idsBig, byte[] metaBig) {
